6f224b100014a9fa6fbacd9d1e48c99f36370f3d
Daniel Varsas <daniel.varsas@gmail.com>
2021-03-10 23:53:16 +0100

    Add MapPage

---
+++ src/pages/MapPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SimpleMap from "../components/SimpleMap";
+
+function MapPage() {
+  return (
+    <div>
+      <SimpleMap />
+    </div>
+  );
+}
+
+export default MapPage;

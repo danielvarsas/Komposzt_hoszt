59468bd18ebaf1e50ab0156086fed931477b1294
Daniel Varsas <daniel.varsas@gmail.com>
2021-03-11 22:51:25 +0100

    Install rwact-select

--- muckhost/src/Pages/Map.js
+++ muckhost/src/Pages/Map.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SimpleMap from "../components/SimpleMap";
+import Filter from "../components/Filter";
 
 function Map() {
   return (
     <div className="flex-container">
-      <div style={{ height: "95vh", width: "60vh" }}>Filters go here</div>
+      <div style={{ height: "95vh", width: "60vh" }}>
+        <Filter />
+      </div>
       <div style={{ width: "110vh" }}>
         <SimpleMap />
       </div>
---
+++ muckhost/src/components/Filter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Filter() {
+  return <Select className="basic-single" />;
+}
+
+export default Filter;
